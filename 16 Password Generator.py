e4f3dbddebbd248c0d4a12629a5fa1d99f8a2286
Timx85 <timx.de.wit@gmail.com>
2019-06-16 10:44:46 +0200

    Update 16 pass

--- 16 Password Generator.py
+++ 16 Password Generator.py
@@ -26,7 +26,7 @@ p =  "".join(random.sample(s,passlen ))
 print(p)
 
 # Or one with asking how many chars the password should be
-def pw_gen(size = 8, chars=string.ascii_letters + string.digits + string.punctuation):
+def pw_gen(size = 8, chars=string.ascii_letters + string.digits): # + string.punctuation
 	return "".join(random.choice(chars) for _ in range(size))
 
 print(pw_gen(int(input('How many characters in your password?'))))
